3d733f9eab832eb52a8f9994d561edd2c49ef2d2
Juan Sebastian <sebassossa02@gmail.com>
2025-04-26 06:56:08 -0500

    🐛 Fix bug in file config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // Usa '/' para rutas absolutas
+  plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',  // Directorio de salida
   }
